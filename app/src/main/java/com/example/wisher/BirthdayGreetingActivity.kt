1394c567b7d3719b124ab9d9fefdb66a63f48daf
Namit-Nayan <namitnayan32@gmail.com>
2021-06-17 14:02:56 +0530

    Migrate from Kotlin synthetics to Jetpack view binding.

--- app/src/main/java/com/example/wisher/BirthdayGreetingActivity.kt
+++ app/src/main/java/com/example/wisher/BirthdayGreetingActivity.kt
@@ -2,19 +2,20 @@ package com.example.wisher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_birthday_greeting.*
+import com.example.wisher.databinding.ActivityBirthdayGreetingBinding
 
 class BirthdayGreetingActivity : AppCompatActivity() {
     companion object {
         const val NAME_EXTRA = "name"
     }
+    private lateinit var binding: ActivityBirthdayGreetingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_birthday_greeting)
-
+        binding = ActivityBirthdayGreetingBinding.inflate(layoutInflater)
         val name = intent.getStringExtra(NAME_EXTRA)
-
-         birthdayGreeting.text = "Happy Birthday \n$name"
+        val wish = getString(R.string.happy_birthday)
+        binding.birthdayGreeting.text = "$wish \n$name"
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/wisher/MainActivity.kt
+++ app/src/main/java/com/example/wisher/MainActivity.kt
@@ -4,16 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.wisher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
-    fun createBirthdayCard(view: View) {
-        val name = nameInput.editableText.toString()
+    fun createBirthdayCard(view : View) {
+        val name = binding.nameInput.editableText.toString()
         val intent = Intent(this, BirthdayGreetingActivity::class.java)
         intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA, name)
         startActivity(intent)
